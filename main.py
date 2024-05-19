654fd57a5c16a25329307931ccd09ab53a9915d3
ryota-iwasakiiwa <r.iwa411@gmail.com>
2024-05-19 10:56:57 +0900

    request body

--- main.py
+++ main.py
@@ -1,10 +1,17 @@
 from enum import Enum
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 class ModelName(str, Enum):
     alexnet = "alexnet"
     resnet = "resnet"
     lenet = "lenet"
+
+class Item(BaseModel):
+    name: str
+    description: str | None = None
+    price: float
+    tax: float | None = None
 
 fake_items_db = [{"item_name": "Foo"}, {"item_name": "Bar"}, {"item_name": "Baz"}]
 
@@ -42,6 +49,21 @@ async def read_user_item(
 @app.get("/items/")
 async def read_item(skip: int = 0, limit: int = 10):
     return fake_items_db[skip : skip + limit]
+
+@app.post("/items/")
+async def create_item(item: Item):
+    item_dict = item.model_dump()
+    if item.tax:
+        price_with_tax = item.price + item.tax
+        item_dict.update({"price_with_tax": price_with_tax})
+    return item_dict
+
+@app.post("/items/{item_id}")
+async def update_item(item_id: int, item: Item, q: str | None = None):
+    result = {"item_id": item_id, **item.model_dump()}
+    if q:
+        result.update({"q": q}) 
+    return result
 
 @app.get("/items/me")
 async def read_item_me():
