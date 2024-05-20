9e6cc6956817aeaa3ad903cea61701ba06d9de57
ryota-iwasakiiwa <r.iwa411@gmail.com>
2024-05-20 23:18:35 +0900

    class as dependencies

---
+++ main_class_as_dependencies.py
@@ -0,0 +1,34 @@
+from fastapi import FastAPI, Depends
+from typing import Annotated
+
+app = FastAPI()
+
+fake_items_db = [{"item_name": "Foo"}, {"item_name": "Bar"}, {"item_name": "Baz"}]
+
+class CommonQueryParams:
+    def __init__(self, q: str | None = None, skip: int = 0, limit: int = 100):
+        self.q = q
+        self.skip = skip
+        self.limit = limit
+
+async def common_parameters(
+    q: str | None = None,
+    skip: int = 0,
+    limit: int = 100
+):
+    return {"q": q, "skip": skip, "limit": limit}
+
+CommonsDep = Annotated[CommonQueryParams, Depends()]
+
+@app.get("/items/")
+async def read_item(commons: CommonsDep):
+    response = {}
+    if commons.q:
+        response.update({"q": commons.q})
+    items = fake_items_db[commons.skip : commons.skip + commons.limit]
+    response.update({"items": items})
+    return response
+
+@app.get("/users/")
+async def read_user(commons: CommonsDep):
+    return commons
