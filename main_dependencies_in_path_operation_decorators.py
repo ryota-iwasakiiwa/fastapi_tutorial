3b1cbe2b8d96dbdcdee2105cc851045349adf00a
ryota-iwasakiiwa <r.iwa411@gmail.com>
2024-05-21 01:02:37 +0900

    dependencies in path operation decorators

---
+++ main_dependencies_in_path_operation_decorators.py
@@ -0,0 +1,17 @@
+from fastapi import FastAPI, Depends, Header, HTTPException, status
+from typing import Annotated
+
+app = FastAPI()
+
+async def verify_token(x_token: Annotated[str, Header()]):
+    if x_token != "fake-super-secret-token":
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="X-Token header invalid")
+    
+async def verify_key(x_key: Annotated[str, Header()]):
+    if x_key != "fake-super-secret-key":
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="X-Key header invalid")
+    return x_key
+
+@app.get("/items/", dependencies=[Depends(verify_token), Depends(verify_key)])
+async def read_items():
+    return [{"item": "Foo"}, {"item": "Bar"}]
