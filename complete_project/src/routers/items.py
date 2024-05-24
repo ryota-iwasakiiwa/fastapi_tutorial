f0d6b11fb290e6eb732b05c1ce5aa6957d4e7cdf
ryota-iwasakiiwa <r.iwa411@gmail.com>
2024-05-24 17:27:08 +0900

    add security in create item

--- complete_project/src/routers/items.py
+++ complete_project/src/routers/items.py
@@ -1,9 +1,11 @@
+from typing import Annotated
 from fastapi import APIRouter, Depends, HTTPException, status
 from sqlalchemy.orm import Session
 
-from ..dependencies import get_db
+from ..dependencies import get_db, get_current_active_user
 from ..crud import crud
 from ..schemas import items as schemas_item
+from ..schemas import users as schemas_user
 
 router = APIRouter(
     prefix="/items",
@@ -11,15 +13,19 @@ router = APIRouter(
 )
 
 
-@router.post("/{user_id}", response_model=schemas_item.Item)
+@router.post("/", response_model=schemas_item.Item)
 def create_item(
-    user_id: int,
     item: schemas_item.ItemCreate,
+    current_user: Annotated[schemas_user, Depends(get_current_active_user)],
     db: Session = Depends(get_db)
 ):
-    return crud.create_user_item(db=db, item=item, user_id=user_id)
+    return crud.create_user_item(db=db, item=item, user_id=current_user.id)
 
 @router.get("/", response_model=list[schemas_item.Item])
-def read_items(skip: int=0, limit: int=100, db: Session = Depends(get_db)):
-    items = crud.get_items(db=db, skip=skip, limt=limit)
+def read_items(
+    skip: int=0,
+    limit: int=100,
+    db: Session = Depends(get_db)
+):
+    items = crud.get_items(db=db, skip=skip, limit=limit)
     return items
